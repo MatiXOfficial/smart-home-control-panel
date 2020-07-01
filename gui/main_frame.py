7e7d9548301a3637685ca85e0cd9f17a819aad55
Mateusz Kocot <mateuszkocot99@gmail.com>
2020-07-01 14:08:36 +0200

    Added settings frame

--- gui/main_frame.py
+++ gui/main_frame.py
@@ -9,12 +9,9 @@ class MainFrame:
     '''
     def __init__(self, config):
 
-        root = tk.Tk()
-        root.resizable(0, 0)
-        self.load_config(config)
-        root.mainloop()
-
-    def load_config(self, config):
+        self.root = tk.Tk()
+        # self.root.resizable(0, 0)
+        self.root.title('Pilot')
 
         # Style przycisków
         style = ttk.Style()
@@ -22,23 +19,23 @@ class MainFrame:
         style.configure('Off.TButton')
 
         # Słownik przycisków
-        buttons = {}
+        self.buttons = {}
+        
+        self.load_config(config)
+        self._info_frame()
+    
+        self.root.mainloop()
 
-        # Funkcje obsługujące przyciski
-        def button_command(room, lamp):
-            if buttons[room][lamp]['text'] == 'Off':
-                buttons[room][lamp]['text'] = 'On'
-                buttons[room][lamp]['style'] = 'On.TButton'
-            else:
-                buttons[room][lamp]['text'] = 'Off'
-                buttons[room][lamp]['style'] = 'Off.TButton'
-
+    def load_config(self, config):
+        '''
+        Funkcja ładująca config, tworzy zakładki i przyciski.
+        '''
         # Tworzenie i wypełnianie zakładek
         tab_control = ttk.Notebook()
 
         for room, lamps in config.items():
             tab = ttk.Frame(tab_control)
-            buttons[room] = {}
+            self.buttons[room] = {}
 
             # Wypisanie nazwy pokoju
             label_title = ttk.Label(tab, text=room, font=('default', 20))
@@ -48,11 +45,38 @@ class MainFrame:
             for i, lamp in enumerate(lamps):
                 label_lamp = ttk.Label(tab, text=lamp, font=('default', 15))
 
-                buttons[room][lamp] = ttk.Button(tab, text='Off', command=lambda x=room, y=lamp: button_command(x, y), style='Off.TButton')
+                self.buttons[room][lamp] = ttk.Button(tab, text='Off', command=lambda x=room, y=lamp: self._button_command(x, y), style='Off.TButton')
 
                 label_lamp.grid(row=i+1, column=0, padx=10, pady=5)
-                buttons[room][lamp].grid(row=i+1, column=1, padx=10)
+                self.buttons[room][lamp].grid(row=i+1, column=1, padx=10)
 
             tab_control.add(tab, text=room)
 
         tab_control.pack(expand=True, fill='both')
+
+    def _button_command(self, room, lamp):
+        '''
+        Funkcja obsługująca przyciski
+        '''
+        if self.buttons[room][lamp]['text'] == 'Off':
+            self.buttons[room][lamp]['text'] = 'On'
+            self.buttons[room][lamp]['style'] = 'On.TButton'
+            self.label_info['text'] = f'Włączono {room}: {lamp}'
+        else:
+            self.buttons[room][lamp]['text'] = 'Off'
+            self.buttons[room][lamp]['style'] = 'Off.TButton'
+            self.label_info['text'] = f'Wyłączono {room}: {lamp}'
+
+    def _info_frame(self):
+        '''
+        Funkcja tworzy ramkę do wyświetlania informacji.
+        '''
+        frame = ttk.Frame(self.root)
+
+        self.label_info = ttk.Label(frame)
+        # button_options = ttk.Button(frame, text='Ustawienia')
+
+        self.label_info.pack(side='left')
+        # button_options.pack(side='right')
+
+        frame.pack(fill='both')
