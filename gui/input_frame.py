ecc8b83399726447cd9005ed28cc76ce483ff6bb
Mateusz Kocot <mateuszkocot99@gmail.com>
2020-07-03 14:58:29 +0200

    Input Frame update

--- gui/input_frame.py
+++ gui/input_frame.py
@@ -11,19 +11,23 @@ class InputFrame():
         self.root.title('Pilot')
         self.root.iconbitmap('images/icon.ico')
         self.root.resizable(False, False)
+        self.root.protocol("WM_DELETE_WINDOW", exit)
 
         label = ttk.Label(self.root, text=f'  {text}: ')
 
         self.text_var = tk.StringVar()
         entry = ttk.Entry(self.root, textvariable=self.text_var)
 
-        button_ok = ttk.Button(self.root, text='Ok', command=self._command_ok)
-        button_cancel = ttk.Button(self.root, text='Anuluj', command=self._command_cancel)
+        frame_buttons = ttk.Frame(self.root)
+        button_ok = ttk.Button(frame_buttons, text='Ok', command=self._command_ok)
+        button_cancel = ttk.Button(frame_buttons, text='Anuluj', command=self._command_cancel)
+
+        button_ok.pack(side='left', anchor='center', padx=10)
+        button_cancel.pack(side='right', anchor='center', padx=10)
 
         label.grid(row=0, column=0, pady=5)
         entry.grid(row=0, column=1, padx=5)
-        button_ok.grid(row=1, column=0, pady=5)
-        button_cancel.grid(row=1, column=1)
+        frame_buttons.grid(row=1, column=0, columnspan=2, pady=2)
 
     def start_and_return(self):
         self.root.mainloop()
